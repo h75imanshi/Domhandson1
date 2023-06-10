2fc69246df3f80ab27ee28ee18ef2597dada72d1
h75imanshi <128072189+h75imanshi@users.noreply.github.com>
2023-06-10 16:39:25 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,12 +1,15 @@
+//guess
 let inputvalue=document.querySelector('input');
+
 let output=document.getElementsByClassName('message');
 let marks=document.getElementsByClassName('score');
+let guess=document.getElementsByClassName('number');
+let higher=document.getElementsByClassName('highscore');
 let randomNum=parseInt(Math.random() * 100+ 1);
 console.log(randomNum);
 
 let counted= 100;
-function maincheck()
-{
+function maincheck(){
     console.log(inputvalue.value);
     if(randomNum>inputvalue.value){
         output[0].innerHTML=("Your guess is Low");
@@ -20,7 +23,14 @@ function maincheck()
         marks[0].innerHTML=(counted);
     }
     else{
-        output[0].innerHTML=("☺☺Hurray you won☺☺");
+        output[0].innerHTML=("😀😀Hurray you won😀😀");
+        guess[0].innerHTML=inputvalue.value;
+       
+        higher[0].innerHTML=(counted);
         marks[0].innerHTML=(counted);
     }
+}
+
+function again(){
+    location.reload('parent')
 }
